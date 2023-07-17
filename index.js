a9b4cd6f3282861d94f97693937aef678458e945
Stefan Espant <stefan.van.der.kort@hva.nl>
2023-07-17 19:57:22 +0200

    v1.0.1
    Eerste versie met data, werkende uren voorspelling

--- index.js
+++ index.js
@@ -1,37 +1,64 @@
 // Importeert express uit de node_modules map
-import express, { request, response } from "express";
+import express from "express";
+import fetch from "node-fetch";
 
-// Maakt een nieuwe express app
 const app = express();
 
-// Importeer de fetch-module
-import fetch from 'node-fetch';
-
-// Stel in hoe express gebruikt kan worden
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.use(express.static("public"));
 
-// Importeert de url met weer data
-const weatherUrl = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m'
+function getTime(dateTimeString) {
+  const dateTime = new Date(dateTimeString);
+  const time = dateTime.toLocaleTimeString("en-GB", {
+    hour: "numeric",
+    minute: "numeric"
+  });
+  return time;
+}
 
-const currentWeatherUrl = weatherUrl + '&current_weather=true'
+// Importeert de url met weer data
+const weatherUrl =
+	"https://api.open-meteo.com/v1/forecast?latitude=52.26&longitude=4.5569&hourly=temperature_2m,dewpoint_2m,rain,visibility,windspeed_10m,winddirection_10m&daily=sunrise,sunset&current_weather=true&timezone=Europe%2FBerlin&forecast_days=1";
+
+const currentWeatherUrl =
+	weatherUrl + "&current_weather=true";
 
 // Maakt een route voor de overzichtspagina
 app.get("/", (request, response) => {
-	fetchJson(currentWeatherUrl).then((data) => {
-		response.render("index", data);
-		console.log(data);
+	fetchJson(weatherUrl).then((data) => {
+		response.render("index");
 	});
 });
 
 // Maakt een route voor de weersverwachtingspagina
-app.get("/forecast", (request, response) => {
-	fetchJson(currentWeatherUrl).then((data) => {
-		response.render("forecast", data);
-		console.log(data);
-	});
-});
+app.get("/forecast", async (request, response) => {
+    const currentWeather = await fetchJson(currentWeatherUrl);
+    const hourlyWeather = await fetchJson(weatherUrl);
+  
+    if (!currentWeather || !hourlyWeather) {
+      // Error handling if the data cannot be fetched
+      response.render("error");
+      return;
+    }
+    const time = currentWeather.hourly.time[0].split("T")[1];
+  
+    const sunriseTime = currentWeather.daily.sunrise[0].split("T")[1];
+    const sunsetTime = currentWeather.daily.sunset[0].split("T")[1];
+  
+    const surfacePressure = currentWeather.hourly.surface_pressure;
+  
+    response.render("forecast", {
+      time: time,
+      sunriseTime: sunriseTime,
+      sunsetTime: sunsetTime,
+      surfacePressure: hourlyWeather.hourly.surface_pressure,
+      current_weather: currentWeather.current_weather,
+      hourly: hourlyWeather.hourly,
+      daily: currentWeather.daily,
+      getTime: getTime // Pass the getTime function to the template
+    });
+  });
 
 // Stelt het poortnummer in waar express op gaat luisteren
 app.set("port", process.env.PORT || 8000);
---
+++ public/script/script.js
@@ -0,0 +1,4 @@
+// Vernieuw de pagina met een tijdsinterval van 1 minuut (60000 milliseconden)
+setInterval(function() {
+  location.reload();
+}, 60000);
